17d31ab24adee53ff9b4e8645e9832f7ac1a92ac
mh.rabbi <rabbichy55gmail.com>
2025-03-02 19:40:31 +0600

    My leetcode solution

---
+++ TwoSum_Leetcode problem no.1.py
@@ -0,0 +1,16 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+def twosum(nums, target):
+    hash_map = {}
+
+    for i, num in enumerate(nums):
+        value = target - num
+
+        if value in hash_map:
+            return [hash_map[value], i]   # return indexes of nums
+        hash_map[num] = i
+
+    return []
+
+print(twosum(nums, target))

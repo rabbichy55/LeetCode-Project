759e3399ab7b14558d57f6cb48318570eedb412b
mh.rabbi <147192267+rabbichy55@users.noreply.github.com>
2025-05-27 00:50:52 +0600

    Binary Search o(log n)

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,22 @@
+# binary search o(nog n)
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums)-1  # right = len(nums)-1 because list index count from 0
+
+        while left <= right:
+            mid = (left + right)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+nums = [1, 3, 5, 6]
+target = 4
+
+solution = Solution()
+result = solution.searchInsert(nums, target)
+print(result)
